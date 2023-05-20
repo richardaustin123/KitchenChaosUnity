e69768195c3145175e27591b443c03a3332f9535
Richard Austin <70338053+richardaustin123@users.noreply.github.com>
2023-05-20 15:39:55 +0100

    Base counter + new simplified pickup

---
+++ BaseCounter.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCounter : MonoBehaviour, IKitchenObjectParent {    
+    
+    [SerializeField] private Transform counterTopPoint;
+
+    private KitchenObject kitchenObject;
+
+    // Interact()
+    public virtual void Interact(Player player) {
+        Debug.Log("Interact() called on BaseCounter");
+    }
+
+    // GetKitchenObjectFollowTransform()
+    public Transform GetKitchenObjectFollowTransform() {
+        return counterTopPoint;
+    }
+
+    // SetKitchenObject()
+    public void SetKitchenObject(KitchenObject kitchenObject) {
+        this.kitchenObject = kitchenObject;
+    }
+
+    // GetKitchenObject()
+    public KitchenObject GetKitchenObject() {
+        return kitchenObject;
+    }
+
+    // ClearKitchenObject()
+    public void ClearKitchenObject() {
+        kitchenObject = null;
+    }
+
+    // HasKitchenObject()
+    public bool HasKitchenObject() {
+        return kitchenObject != null;
+    }
+}
