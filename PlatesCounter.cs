f1b1306dbd01fa3f0e2f371f792ed0766fe96e90
Richard Austin <70338053+richardaustin123@users.noreply.github.com>
2023-05-28 12:30:24 +0100

    Plates spawn
    Plates now spawn on a plates counter. Max spawn and spawn speed defined in code. Player can pick up plate and new lates will spawn on the counter. Plates spawn on top of each other

---
+++ PlatesCounter.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatesCounter : BaseCounter {
+
+    // public event EventHandler<OnPlateSpawnedEventArgs> OnPlateSpawned;
+    public event EventHandler OnPlateSpawned;
+    public event EventHandler OnPlateRemoved;
+
+    [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
+
+    private float spawnPlateTimer;
+    private float spawnPlateTimerMax = 2f;
+    private int plateSpawnedAmount;
+    private int plateSpawnedAmountMax = 4;
+
+    private void Update() {
+        spawnPlateTimer += Time.deltaTime;
+        if (spawnPlateTimer > spawnPlateTimerMax) {
+            spawnPlateTimer = 0f;
+
+            if (plateSpawnedAmount < plateSpawnedAmountMax) {
+                plateSpawnedAmount++;
+
+                OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+                // OnPlateSpawned?.Invoke(this, new OnPlateSpawnedEventArgs(plateKitchenObjectSO));
+                // SpawnPlate();
+            }
+        }
+    }
+
+    public override void Interact(Player player) {
+        if (!player.HasKitchenObject()) {
+            // Player is not carrying anything
+            if (plateSpawnedAmount > 0) {
+                // There is a plate on the counter
+                // Pick up the plate
+                plateSpawnedAmount--;
+
+                KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, player);
+
+                OnPlateRemoved?.Invoke(this, EventArgs.Empty);
+            } else {
+                // There is no plate on the counter
+                // Do nothing
+            }
+        }
+    }
+}
