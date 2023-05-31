219a77ce415b104bad275b7f5702d51a7a9caa61
Richard Austin <70338053+richardaustin123@users.noreply.github.com>
2023-05-31 18:15:48 +0100

    UI for recipes
    UI added for recipes that spawn in and displays correct icons for the dish

--- DeliveryManager.cs
+++ DeliveryManager.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DeliveryManager : MonoBehaviour {
+
+    public event EventHandler OnRecipeSpawned;
+    public event EventHandler OnRecipeCompleted;
 
     public static DeliveryManager Instance { get; private set; }
 
@@ -24,9 +28,10 @@ public class DeliveryManager : MonoBehaviour {
             spawnRecipeTimer = spawnRecipeTimerMax;
 
             if (waitingRecipeSOList.Count < waitingRecipesMax) {
-                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[Random.Range(0, recipeListSO.recipeSOList.Count)];
-                Debug.Log("Waiting recipe: " + waitingRecipeSO.recipeName);
+                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[UnityEngine.Random.Range(0, recipeListSO.recipeSOList.Count)];
                 waitingRecipeSOList.Add(waitingRecipeSO);
+
+                OnRecipeSpawned?.Invoke(this, EventArgs.Empty);
             }
         }
     }
@@ -60,12 +65,17 @@ public class DeliveryManager : MonoBehaviour {
                     // Player delivered the correct recipe!
                     Debug.Log("Correct recipe delivered: " + waitingRecipeSO.recipeName);
                     waitingRecipeSOList.RemoveAt(i);
+
+                    OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
                     return;
                 }
             }
         }
         // No matches found
         // Player delivered the wrong recipe!
-        Debug.Log("Wrong recipe delivered");
+    }
+
+    public List<RecipeSO> GetWaitingRecipeSOList() {
+        return waitingRecipeSOList;
     }
 }
