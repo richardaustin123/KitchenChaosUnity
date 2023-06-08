fb4c17d903e43bd3c58948bc427d848a4e27d261
Richard Austin <70338053+richardaustin123@users.noreply.github.com>
2023-06-08 19:38:02 +0100

    Tutorial UI (start, unfinished)

---
+++ TutorialUI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TutorialUI : MonoBehaviour {
+
+    [SerializeField] private TextMeshProUGUI keyMoveUpText;
+    [SerializeField] private TextMeshProUGUI keyMoveDownText;
+    [SerializeField] private TextMeshProUGUI keyMoveLeftText;
+    [SerializeField] private TextMeshProUGUI keyMoveRightText;
+    [SerializeField] private TextMeshProUGUI keyInteractText;
+    [SerializeField] private TextMeshProUGUI keyInteractAlternateText;
+    [SerializeField] private TextMeshProUGUI keyPauseText;
+    [SerializeField] private TextMeshProUGUI keyGamepadInteractText;
+    [SerializeField] private TextMeshProUGUI keyGamepadInteractAlternateText;
+    [SerializeField] private TextMeshProUGUI keyGamepadPauseText;
+
+}
